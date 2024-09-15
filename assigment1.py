4a0f30b75cea13a3437a1b0bc207a8735f7458f1
sidpcode19 <siddhipateriya19@gmail.com>
2024-09-15 16:06:36 +0530

    Assigment1.py

--- assigment1.py
+++ assigment1.py
@@ -1,7 +1,7 @@
-# 1. Write a Python program to print the following string in a specific format (see the
-# output). Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above
-# the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what
-# you are".
+'''1. Write a Python program to print the following string in a specific format (see the
+output). Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above
+the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what
+you are".'''
 
 print("Twinkle,Twinkle, little star.")
 print("How I wonder what you are!")
@@ -12,8 +12,8 @@ print("How I wonder what you are!")
 
 
 
-# 2. Write a Python program that accepts the user's first and last name and prints them in
-# reverse order with a space between them.
+'''2. Write a Python program that accepts the user's first and last name and prints them in
+reverse order with a space between them.'''
 
 fName = input("Enter first name:")
 lName = input("Enter last name: ")
@@ -21,8 +21,8 @@ print(lName,fName)
 
 
 
-# 3. Write a Python program that calculates the area of a circle based on the radius entered
-# by the user.
+'''3. Write a Python program that calculates the area of a circle based on the radius entered
+by the user.'''
 
 r = int(input("Enter the radius : "))
 Area = 3.14*r*r
@@ -30,8 +30,8 @@ print(Area)
 
 
 
-# 4. Write a Python program to display the first and last colors from the following list.
-# color_list = ["Red","Green","White" ,"Black"]
+'''4. Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]'''
 
 color_list = ["Red","Green","White","Black"]
 print(color_list[0])
@@ -39,9 +39,9 @@ print(color_list[3])
 
 
 
-# 5. Write a Python program that accepts an integer (n) and computes the value of
-# n+nn+nnn.
-# Sample value of n is 5
+'''5. Write a Python program that accepts an integer (n) and computes the value of
+n+nn+nnn.
+Sample value of n is 5'''
 
 n = int(input("Enter the number: "))
 ones = n
@@ -52,8 +52,8 @@ print(add)
 
 
 
-# 6. Write a Python program that accepts a sequence of comma-separated numbers from the
-# user and generates a list and a tuple of those numbers. Sample data : 3, 5, 7, 23
+'''6. Write a Python program that accepts a sequence of comma-separated numbers from the
+user and generates a list and a tuple of those numbers. Sample data : 3, 5, 7, 23'''
 
 x = str(input("Enter the number: ")).split(",")
 print(x)
@@ -62,7 +62,7 @@ print(x)
 
 
 
-# 7. Write a program that will convert celsius value to Fahrenheit.
+'''7. Write a program that will convert celsius value to Fahrenheit.'''
 
 celsius = float(input("Enetr temperature: "))
 fahrenheit = 1.8*celsius + 32
@@ -71,8 +71,8 @@ print(fahrenheit)
 
 
 
-# 8. User will input (2numbers). Write a program to swap the numbers. Add incrementally
-# in any one variable.
+'''8. User will input (2numbers). Write a program to swap the numbers. Add incrementally
+in any one variable.'''
 
 first = int(input("Enter the first number: "))
 second = int(input("Enter the second number: "))
@@ -85,7 +85,7 @@ print(first,second)
 
 
 
-# 9. Write a program that will tell whether the number entered by the user is odd or even.
+'''9. Write a program that will tell whether the number entered by the user is odd or even.'''
 
 n = int(input("Enter the number: "))
 if (n%2 == 0) : print("even number.")
@@ -93,7 +93,7 @@ else : print("odd number.")
 
 
 
-# 10. Write a program that will tell whether the given year is a leap year or not.
+'''10. Write a program that will tell whether the given year is a leap year or not.'''
 
 year = int(input("Enter the year"))
 
@@ -105,7 +105,7 @@ else:
     print("is not a leap year")
 
 
-# 11. Write a program to find the euclidean distance between two coordinates.
+'''11. Write a program to find the euclidean distance between two coordinates.'''
 
 
 x1,y1 = int(input("x1:")), int(input("y1:"))
@@ -115,8 +115,8 @@ print(distance)
 
 
 
-# 12. Write a program that take a user input of three angles and will find out whether it can
-# form a triangle or not.
+'''12. Write a program that take a user input of three angles and will find out whether it can
+form a triangle or not.'''
 
 a1, a2, a3 = float(input("a1:")),float(input("a2:")),float(input("a3:"))
 sum = a1 + a2 + a3
@@ -125,7 +125,7 @@ else : print("This is not a triangle.")
 
 
 
-# 13. WAP to find compound interest for given values.
+'''13. WAP to find compound interest for given values.'''
 
 p ,r ,t,n  = int(input("principle: ")), int(input("rate: ")), int(input("time: ")), int(input(number of time))
 CI = p*((1+(r/n))**(t*n))
@@ -133,8 +133,8 @@ print(CI)
 
 
 
-# 14. Given a positive integer N, the task is to write a Python program to check if the number
-# is Prime or not in Python.
+'''14. Given a positive integer N, the task is to write a Python program to check if the number
+is Prime or not in Python.'''
 
 N = int(input("Enter the number : "))
 c = 0
@@ -147,7 +147,7 @@ else:
 print("Prime")
 
 
-# 15. Given a positive integer N. The task is to find 12 + 22 + 32 + ….. + N2.
+'''15. Given a positive integer N. The task is to find 12 + 22 + 32 + ….. + N2.'''
 
 n = int(input("Enter the number: "))
 sum = 0
